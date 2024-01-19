a12079cd97b82015f38ddda618faca23ead82695
henriqueolivgp <henrique.o@ipvc.pt>
2024-01-19 00:25:04 +0000

    first commit

---
+++ FocusBeats/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello i'm a FocusBeats for your study!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
